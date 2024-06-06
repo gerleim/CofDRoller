d145b94e1dd3d1f827aaa096a49bbd24de566839
gerleim <gerleim@gmail.com>
2024-06-06 09:32:24 +0200

    Add ExtendedActionResults Test. Refactor.

---
+++ Tests/CofdRoller.CofdRollerTests/ExtendedAction/ExtendedActionResultsBaseTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CofdRoller.Tests;
+
+[TestClass]
+public class ExtendedActionResultsBaseTests
+{
+    public class ExtendedActionResultsBaseTest : ExtendedActionResultsBase
+    {
+    }
+
+    [TestMethod]
+    public void ToStringTest()
+    {
+        var result = new ExtendedActionResultsBaseTest();
+        var rollResulst1 = new RollResults();
+        rollResulst1.Add(new SingleRollResult(2, []));
+        result.Add(new Result(1, rollResulst1));
+        Assert.AreEqual(2, result.Successes);
+
+        var rollResulst2 = new RollResults();
+        rollResulst2.Add(new SingleRollResult(3, []));
+        result.Add(new Result(1, rollResulst2));
+        Assert.AreEqual(5, result.Successes);
+    }
+}
